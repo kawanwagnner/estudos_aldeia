5b8d2473c79c371c11c547bd60e5a1fd7d8a0fe2
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-10-25 15:37:50 -0300

    day changes

--- src/js/menuMobile.js
+++ src/js/menuMobile.js
@@ -2,6 +2,29 @@ const btn = document.querySelector('#btnMenu');
 
 btn.addEventListener('click', () => {
     const navBar = document.querySelector('.navbar'); 
+    const headerPage = document.getElementsByTagName('header')[0]; 
+    const tegAmenu = document.querySelector('.linkNavBar'); 
 
-    navBar.classList.toggle('active')
+    navBar.classList.toggle('active'); 
+    headerPage.classList.toggle('changeColorsHeader')
+    btn.classList.toggle('changeColorButton'); 
+    headerPage.classList.toggle('colorLinkMenu'); 
+    tegAmenu.classList.toggle('colorLinkMenu')
+    
+    if (btn.innerHTML == 'X') {
+        btn.innerHTML = '<i class="fa-solid fa-bars"></i>'; 
+    } else {
+        btn.innerHTML = 'X'; 
+    }
+})
+
+
+/* change theme */
+
+const btnChangeTheme = document.querySelector('#change_theme')
+
+btnChangeTheme.addEventListener('click', () => {
+    const bodyPage = document.body; 
+    document.querySelectorAll('.hide').forEach(item => item.style.display = 'none')
+    bodyPage.classList.toggle('changeMode'); 
 })
